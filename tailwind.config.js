2d9ebdac0c96be4717cd5f73ca04275574418724
Md Anas Sabah <mdanassabah@gmail.com>
2023-09-17 22:28:43 +0530

    update: added new icons and components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,25 @@ module.exports = {
     extend: {
       colors: {
         "blue-20": "rgba(29, 155, 240, 0.24)",
-        "neutral-50": "#f9f9f9",
-        "neutral-1000": "#000000",
+        "twitter-blue": "#1D9BF0",
+        "blue-wash": "rgba(117,190,239,0.2)",
+        "button-stroke": "#546A7A",
+        "searchbar-fill": "#212327",
+        "card-fill": "#16181C",
+        success: "#00BE74",
+        error: "#8B141A",
+        "neutral-50": "#F9F9F9",
+        "neutral-100": "#F4F4F4",
+        "neutral-200": "#E4E4E4",
+        "neutral-300": "#D3D3D3",
+        "neutral-400": "#A2A2A2",
+        "neutral-500": "#737373",
+        "neutral-600": "#525252",
+        "neutral-700": "#404040",
+        "neutral-800": "#262626",
+        "neutral-900": "#171717",
+        "neutral-950": "#0A0A0A",
+        "neutral-1000": "#000",
       },
       boxShadow: {
         custom: "0px 8px 16px 0px rgba(0, 0, 0, 0.25);",
@@ -23,6 +40,7 @@ module.exports = {
       },
       fontSize: {
         2.5: "26px",
+        48: "48px",
       },
       width: {
         155.5: "155.5px",
